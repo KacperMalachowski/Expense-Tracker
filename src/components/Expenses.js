b697b75d055157d4cd3daf13494e7cc53d88ad7f
Kacper Małachowski <kacpermaachowskI@gmail.com>
2022-01-11 23:34:15 +0100

    Add Expensees.js which renders expenses list

---
+++ src/components/Expenses.js
@@ -0,0 +1,18 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+
+function Expenses({ expenses }) {
+  return (
+    <div className="expenses">
+      {expenses.map(item =>
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      )}
+    </div>
+  )
+}
+
+export default Expenses;
