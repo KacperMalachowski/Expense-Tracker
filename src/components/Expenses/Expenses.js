dba6bf6061f3a7854c42a3f8246196f22b937764
Kacper Małachowski <kacpermaachowskI@gmail.com>
2022-01-13 13:48:07 +0100

    Add id to each ExpenseItem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import "./Expenses.css";
 const Expenses = ({ expenses }) => (
   <Card className="expenses">
     {expenses.map((item) => (
-      <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />
     ))}
   </Card>
 );
